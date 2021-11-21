5bfdca01553409fcfb7955a10d02f50a219f0aec
sosaro <93305779+sosaro@users.noreply.github.com>
2021-11-21 09:45:52 +0100

    Update examen1ev.java

--- examen1ev.java
+++ examen1ev.java
@@ -1,1 +1,18 @@
 // Examen 1EV EDD
+
+public class examen1ev {
+  
+  public static void main(String[] args) {
+  
+    try {
+
+        for (int i =0 ; i <= 10; i++) {
+          int resultado = 7*i;
+          System.out.println(7 + " * " + i + " = " + resultado);
+        }
+
+    } catch (Exception e){
+        e.printStackTrace();
+      }
+  }
+}
